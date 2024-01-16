4eac557e9fc5ce239f4d2705b3497e99bbaf4a4b
saurabh patil <saurabhspatil4545@gmail.com>
2024-01-16 14:51:48 +0530

    merged chatbot and login/logout functionality

--- Backend/routes/routes.js
+++ Backend/routes/routes.js
@@ -1,7 +1,7 @@
 import { slogin } from "../controller/student-contoller.js";
 import { slist, tlogin } from "../controller/teacher-controller.js";
 import { plogin } from "../controller/parent-controller.js";
-import { quiz, currentsciece } from "../controller/quiz-contoller.js";
+import { quiz } from "../controller/quiz-contoller.js";
 import { studentdata,cResult ,Usubjects,Dusubjects,dResult} from "../controller/quiz-contoller.js";
 
 import express from "express";
@@ -13,7 +13,7 @@ route.post('/slist', slist);
 route.post('/tlogin', tlogin);
 route.post('/plogin', plogin);
 route.get('/quiz', quiz);
-route.get('/cscience',currentsciece)
+route.get('/cscience')
 
 
 route.post('/studentdata', studentdata);
